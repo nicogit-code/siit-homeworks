67c40f999fa7840a56b30387189022fa7f6f9c87
Nicoleta C <nicoleta.cauneac@gmail.com>
2020-04-02 15:59:34 +0300

    Tema React

--- src/components/movies/MovieDetails.js
+++ src/components/movies/MovieDetails.js
@@ -18,10 +18,37 @@ function MovieDetails() {
     if(movie) {
         console.log(movie);
         return (
-            <>
-                <h1>{ movie.Title }</h1>
-                <h5>Year: { movie.Year }</h5>
-            </>
+            <div className="container">
+                <div className="row">
+                    <div className="col-9">
+                        <h1>{ movie.Title }</h1>
+                        <h3>({ movie.Year })</h3>
+                    </div>
+
+                    <div className="col-3">
+                        <h5>IMDb Rating: { movie.imdbRating } /10</h5>
+                        <p>{ movie.imdbVotes }</p>
+                    </div>
+                </div>
+
+                <div className="row">
+                        <p><b>Genre:</b> { movie.Genre } |</p>
+                        <p><b>Runtime:</b> { movie.Runtime } |</p>
+                        <p><b>Released:</b> { movie.Released }</p>
+                </div>
+
+                <div className="col-9">
+                    <img className="card-img-top" src={ movie.Poster } alt="Movie Poster" />
+                </div>
+
+                <div className="col-9">
+                    <p><b>Movie plot:</b> { movie.Plot }</p>
+                    <p><b>Director:</b> { movie.Director }</p>
+                    <p><b>Writers:</b> { movie.Writer }</p>
+                    <p><b>Stars:</b> { movie.Actors }</p>
+                </div>
+                
+            </div>
         );
     } else {
         return <h1>Loading ...</h1>;
